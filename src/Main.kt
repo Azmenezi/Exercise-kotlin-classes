d12df9e14e9f60a72763439bbe060322b037fdeb
azmenezi <azmenezi@gmail.com>
2025-02-16 17:32:02 +0300

    new book classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World!")
+    val book = Book()
+    book.title = "harry potter"
+    book.author = "humoud alghanim"
+    book.yearPublished = 2023
+
+    println(book)
+}
+
+class Book {
+    var title: String = ""
+    var author: String = ""
+    var yearPublished: Int = 1900
 }
